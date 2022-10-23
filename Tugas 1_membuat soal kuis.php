751a716f8eb4df8e8668e7d2d3e8a3fb9b648af8
ErikaRamadanti12 <106233943+ErikaRamadanti12@users.noreply.github.com>
2022-10-23 18:04:44 +0700

    Add files via upload

---
+++ Tugas 1_membuat soal kuis.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h6> Proses langkah pembuatan aplikasi berbasis web yang dilalui dengan tahapteknologi jaringan internet, dan bahasa pemograman adalah</h6>
+<p>A. Pemograman c++</p>
+<p>B. Algoritma pemograman</p>
+<p>C. Pemograman web</p>
+<p>D.Web Desain</p>
+<p>E.Pemograman mobile</p>
+
+<?php
+$nilai = "C" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda salah";
+    break;
+  case "B":
+    echo "Jawaban anda salah";
+    break;
+  case "C":
+    echo "Jawaban anda benar";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+<h6> 
+File Kepanjangan dari HTML adalah.... </h6>
+<p>A. Hyper Text Markup Language c++</p>
+<p>B. Hyper Link Mobile Language </p>
+<p>C. Hyper Text Markup Languenge</p>
+<p>D. Hyper This Mobile Lose</p>
+<p>E. Hyper Tell Markup Languenge</p>
+
+<?php
+$nilai = "A" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda benar";
+    break;
+  case "B":
+    echo "Jawaban anda salah";
+    break;
+  case "C":
+    echo "Jawaban anda salah";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+<h6> 
+File HTML harus berekstensi.... </h6>
+<p>A. php c++</p>
+<p>B. html </p>
+<p>C. index</p>
+<p>D. css</p>
+<p>E. png</p>
+
+<?php
+$nilai = "B" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda salah";
+    break;
+  case "B":
+    echo "Jawaban anda benar";
+    break;
+  case "C":
+    echo "Jawaban anda salah";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+<h6> 
+Tag HTML yang memiliki peran penting untuk menunjukan bagian halaman web adalah.... </h6>
+<p>A. Heading c++</p>
+<p>B. Body </p>
+<p>C. Table</p>
+<p>D. Row</p>
+<p>E. Tab</p>
+
+<?php
+$nilai = "A" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda benar";
+    break;
+  case "B":
+    echo "Jawaban anda salah";
+    break;
+  case "C":
+    echo "Jawaban anda salah";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+ 
+<h6> 
+Table tag <tr> pada HTML saat membuat tabel berfungsi untuk.... </h6>
+<p>A. Membuat kolom</p>
+<p>B. Membuat baris </p>
+<p>C. Membuat judul</p>
+<p>D. Membuat list</p>
+<p>E. Membuat baris baru</p>
+
+<?php
+$nilai = "B" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda salah";
+    break;
+  case "B":
+    echo "Jawaban anda benar";
+    break;
+  case "C":
+    echo "Jawaban anda salah";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+<h6> 
+List yang dibuat di dalam list adalah.... </h6>
+<p>A. Nested list</p>
+<p>B. Ordered list</p>
+<p>C. Navigasi</p>
+<p>D. Description list</p>
+<p>E. PHP</p>
+
+<?php
+$nilai = "A" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda benar";
+    break;
+  case "B":
+    echo "Jawaban anda salah";
+    break;
+  case "C":
+    echo "Jawaban anda salah";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+<h6> 
+Bahasa khusus yang berfungsi untuk mempercantik atau memperindah tampilan web.... </h6>
+<p>A. CSS</p>
+<p>B. Laravel</p>
+<p>C. Codeigniter</p>
+<p>D. PHP</p>
+<p>E. Javascipt</p>
+
+<?php
+$nilai = "A" ;
+
+switch ($nilai) {
+  case "A":
+    echo "Jawaban anda benar";
+    break;
+  case "B":
+    echo "Jawaban anda salah";
+    break;
+  case "C":
+    echo "Jawaban anda salah";
+    break;
+  case "D": 
+    echo "Jawaban anda salah";
+    break;
+  case "E":
+  	echo "Jawaban anda salah";
+    break;
+  default:
+    echo "Masukan jawaban yang valid";
+    
+}
+?>
+ <h6> Method dibagi menjadi dua, yaitu....</h6>
+<p>A. Get dan action</p>
+<p>B. Submit dan aksi</p>
+<p>C. Method dan get</p>
+<p>D. Get dan post</p>
+<p>E. Post dan method</p>
+
+<?php
+$t = "D";
+
+if ($t = "D") {
+  echo "jawaban anda benar";
+}
+?>
+</body>
+</html>
+<h6> Button khusus untuk mengirimkan data disebut....</h6>
+<p>A. Action</p>
+<p>B. Submit</p>
+<p>C. Method</p>
+<p>D. Password</p>
+<p>E. Text</p>
+
+<?php
+$t = "B";
+
+if ($t = "B") {
+  echo "jawaban anda benar";
+}
+?>
+<h6> Atribut yang digunakan untuk memberikan nilai pada suatu elemen adalah....</h6>
+<p>A. Get</p>
+<p>B. Post</p>
+<p>C. Value</p>
+<p>D. Placeholder</p>
+<p>E. Img</p>
+
+<?php
+$t = "C";
+
+if ($t = "C") {
+  echo "jawaban anda benar";
+}
+?>
